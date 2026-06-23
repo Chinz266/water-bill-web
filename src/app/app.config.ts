99e51df1d09dd044f4442f75eb0c509676bb5a1b
TitleKung-01 <dev.meoww@gmail.com>
2026-06-24 03:46:03 +0700

    📦 : fix error zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // 🌟 เพิ่มบรรทัดนี้
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient() // 🌟 เพิ่มฟังก์ชันนี้เข้าไป
+    provideHttpClient()
   ]
 };
